618d571a1c03941fd8bff6118a9feec7c7bf5e29
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2015-12-16 14:54:24 +0000

    Use headers expliciytly - public ifaces not insatlled!

--- MySQLTestApp/src/MySQLTestMain.cpp
+++ MySQLTestApp/src/MySQLTestMain.cpp
@@ -10,7 +10,17 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <mysql_public_iface.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/warning.h>
+#include <cppconn/metadata.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
+#include <cppconn/resultset_metadata.h>
+#include <cppconn/statement.h>
+#include "mysql_driver.h"
+#include "mysql_connection.h"
+
 
 #include "epicsExit.h"
 #include "epicsThread.h"
